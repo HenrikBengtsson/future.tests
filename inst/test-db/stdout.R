c786eebcd9600343fb71807afeea11e42a5e1ee8
Henrik Bengtsson <hb@aroma-project.org>
2018-11-07 15:29:33 -0800

    More prototype tests

---
+++ inst/test-db/stdout.R
@@ -0,0 +1,67 @@
+make_test(.title = "future() - standard output", stdout = c(FALSE, TRUE), .expr = {
+  truth_rows <- utils::capture.output({
+    print(1:50)
+    str(1:50)
+    cat(letters, sep = "-")
+    cat(1:6, collapse = "\n")
+    write.table(datasets::iris[1:10,], sep = "\t")
+  })
+  truth <- paste0(paste(truth_rows, collapse = "\n"), "\n")
+  print(truth)
+  
+  f <- future({
+    print(1:50)
+    str(1:50)
+    cat(letters, sep = "-")
+    cat(1:6, collapse = "\n")
+    write.table(datasets::iris[1:10,], sep = "\t")
+    42L
+  }, stdout = stdout)
+  
+  r <- result(f)
+  str(r)
+  stopifnot(value(f) == 42L)
+  
+  if (stdout) {
+    print(r)
+    message(sprintf("- stdout = %s", stdout))
+    stopifnot(identical(r$stdout, truth))
+  } else {
+    print(r)
+    print(plan())
+    message(sprintf("- stdout = %s", stdout))
+    stopifnot(is.null(r$stdout))
+  }
+})
+
+
+make_test(.title = "%<-% - standard output", stdout = c(FALSE, TRUE), .expr = {
+  truth_rows <- utils::capture.output({
+    print(1:50)
+    str(1:50)
+    cat(letters, sep = "-")
+    cat(1:6, collapse = "\n")
+    write.table(datasets::iris[1:10,], sep = "\t")
+  })
+  truth <- paste0(paste(truth_rows, collapse = "\n"), "\n")
+  print(truth)
+  
+  v %<-% {
+    print(1:50)
+    str(1:50)
+    cat(letters, sep = "-")
+    cat(1:6, collapse = "\n")
+    write.table(datasets::iris[1:10,], sep = "\t")
+    42L
+  } %stdout% stdout
+  
+  out <- utils::capture.output(y <- v)
+  
+  stopifnot(y == 42L)
+  if (!stdout) {
+    stopifnot(out == "")
+  } else {
+    print(out)
+    stopifnot(identical(out, truth_rows))
+  }
+})
--- tests/Test-class.R
+++ tests/Test-class.R
@@ -1,37 +1,25 @@
 library(future.tests)
 library(future)
 
-tests <- load_tests()
-message("Number of tests: ", length(tests))
-
-message("Running all tests ...")
-
-library(future)
-
 lazy <- FALSE
 global <- TRUE
+stdout <- TRUE
 value <- TRUE
 recursive <- FALSE
 
-ntests <- length(tests)
+tests <- load_tests()
+message("Number of tests: ", length(tests))
 
-for (lazy in c(FALSE, TRUE)) {
-  message(sprintf("Running tests that supports lazy = %s ...", lazy))
+message("Run first three tests ...")
 
-  tests_tt <- subset_tests_by_args(tests, args = list(lazy = lazy))
-  ntests_tt <- length(tests_tt)
-  message(sprintf(" - Number of tests: %d out of %d", ntests_tt, ntests))
+library(future)
+res <- run_tests(head(tests, 3L))
+print(res)
 
-  res_tt <- run_tests(tests_tt)
-  print(res_tt)
-  
-  message(sprintf("Running tests that supports lazy = %s ... OK", lazy))
-}
-
-message("Running all tests ... DONE")
+message("Run first three tests ... DONE")
 
 
-message("Running all tests across different future plans ...")
+message("Run all tests across different future plans ...")
 
 library(future)
 
@@ -51,25 +39,26 @@ res <- along_test_plans({
   res_plan <- list()
   for (lazy in c(FALSE, TRUE)) {
     for (global in c(FALSE, TRUE)) {
-      args <- c(lazy = lazy, global = global)
-      args_tag <- paste(sprintf("%s=%s", names(args), args), collapse = ",")
-      message(sprintf("Running tests with (%s) ...", args_tag))
-  
-      tests_tt <- subset_tests_by_args(tests, args = args)
-      ntests_tt <- length(tests_tt)
-      message(sprintf(" - Number of tests: %d out of %d", ntests_tt, ntests))
+      for (stdout in c(FALSE, TRUE)) {
+        args <- c(lazy = lazy, global = global, stdout = stdout)
+        args_tag <- paste(sprintf("%s=%s", names(args), args), collapse = ",")
+        message(sprintf("Running tests with (%s) ...", args_tag))
     
-      res_tt <- run_tests(tests_tt)
-      print(res_tt)
+        tests_tt <- subset_tests_by_args(tests, args = args)
+        ntests_tt <- length(tests_tt)
+        message(sprintf(" - Number of tests: %d out of %d", ntests_tt, ntests))
+      
+        res_tt <- run_tests(tests_tt)
+        print(res_tt)
+    
+        res_plan[[args_tag]] <- res_tt
   
-      res_plan[[args_tag]] <- res_tt
-
-      message(sprintf("Running tests with (%s) ... DONE", args_tag))
+        message(sprintf("Running tests with (%s) ... DONE", args_tag))
+      }
     }
   }
-
   res_plan
 })
 print(res)
 
-message("Running all tests across different future plans ... DONE")
+message("Run all tests across different future plans ... DONE")
