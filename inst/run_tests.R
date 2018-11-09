d287be69f855a92693ca34f27c1e5651dadbdf3b
Henrik Bengtsson <hb@aroma-project.org>
2018-11-08 17:51:49 -0800

    Running prototype tests under sequential and multisession:2 plans

--- inst/run_tests.R
+++ inst/run_tests.R
@@ -7,31 +7,47 @@ print(tests_tbl)
 message("Number of tests: ", length(tests))
 message("Minimal number of test combinations: ", nrow(tests_tbl))
 
+
 message("Run all tests ...")
 
-library(future)
+message("- Future backends:")
+add_test_plan(plan(sequential))
+add_test_plan(plan(multisession, workers = 2L))
+test_plans <- test_plans()
+print(test_plans)
 
 value <- TRUE
 recursive <- FALSE
 
 defaults <- list(lazy = FALSE, globals = TRUE, stdout = TRUE)
 
-for (lazy in c(FALSE, TRUE)) {
-  for (globals in c(TRUE, FALSE)) {
-    for (stdout in c(TRUE, FALSE)) {
-      args <- list(lazy = lazy, globals = globals, stdout = stdout)
-      args_tag <- paste(sprintf("%s=%s", names(args), unlist(args)), collapse = ", ")
-      cat(sprintf("Arguments (%s):\n", args_tag))
-      
-      tests_t <- subset_tests(tests, args = args, defaults = defaults)
-#      print(do.call(rbind, tests_t))
-
-      results <- run_tests(tests_t, defaults = defaults)
-      df_results <- do.call(rbind, results)
-#      df_results <- subset(df_results, !is.na(success))
-      print(df_results)
+for (pp in seq_along(test_plans)) {
+  test_plan <- test_plans[[pp]]
+  eval(test_plan)
+  print(plan())
+  
+  for (lazy in c(FALSE, TRUE)) {
+    for (globals in c(TRUE, FALSE)) {
+      for (stdout in c(TRUE, FALSE)) {
+        args <- list(lazy = lazy, globals = globals, stdout = stdout)
+        args_tag <- paste(sprintf("%s=%s", names(args), unlist(args)), collapse = ", ")
+	
+        cat(sprintf("Arguments (%s):\n", args_tag))
+        print(plan())
+        
+        tests_t <- subset_tests(tests, args = args, defaults = defaults)
+  #      print(do.call(rbind, tests_t))
+  
+        results <- run_tests(tests_t, defaults = defaults)
+        df_results <- do.call(rbind, results)
+  #      df_results <- subset(df_results, !is.na(success))
+        print(df_results)
+      }
     }
   }
-}
+
+  ## Shutdown current plan
+  plan(sequential)
+} ## for (pp ...)
 
 message("Run all tests ... DONE")
