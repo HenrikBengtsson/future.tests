a91afdd66d50af92b45c0e4a90225aa0287104a9
Henrik Bengtsson <hb@aroma-project.org>
2018-12-01 20:37:26 -0800

    FIX: nbrOfWorkers() message assumed an integer but not +Inf

--- inst/test-db/nbrOfWorkers.R
+++ inst/test-db/nbrOfWorkers.R
@@ -1,5 +1,5 @@
 make_test(title = "nbrOfWorkers()", tags = c("nbrOfWorkers"), {
   n <- nbrOfWorkers()
-  message(sprintf("nbrOfWorkers: %d", n))
+  message(sprintf("nbrOfWorkers: %g", n))
   stopifnot(is.numeric(n), length(n) == 1L, n >= 1L)
 })
