eb07304f47f31a6f22d62a1bbe9df76e9607f546
Henrik Bengtsson <hb@aroma-project.org>
2019-04-20 17:25:07 -0700

    TESTS: Add tests for early-signalling/relaying of warnings and messages

--- inst/test-db/early-signaling.R
+++ inst/test-db/early-signaling.R
@@ -6,7 +6,7 @@ make_test(title = "Early signaling of errors", args = list(lazy = c(FALSE, TRUE)
     r <- resolved(f)
   
     v <- tryCatch(value(f), error = identity)
-    stopifnot(inherits(v, "error"))
+    stopifnot(inherits(v, "error"), conditionMessage(v) == "bang!")
 
     TRUE
   }, error = identity)
@@ -15,24 +15,42 @@ make_test(title = "Early signaling of errors", args = list(lazy = c(FALSE, TRUE)
 })
 
 
-##   ## Warnings
-##   f <- future({ warning("careful!") }, lazy = TRUE)
-##   res <- tryCatch({
-##     r <- resolved(f)
-##   }, condition = function(w) w)
-##   str(res)
-##   stopifnot(inherits(res, "warning"))
-##   
-##   ## Messages
-##   f <- future({ message("hey!") }, lazy = TRUE)
-##   res <- tryCatch({
-##     r <- resolved(f)
-##   }, condition = function(w) w)
-##   stopifnot(inherits(res, "message"))
-##   
-##   ## Condition
-##   f <- future({ signalCondition(simpleCondition("hmm")) }, lazy = TRUE)
-##   res <- tryCatch({
-##     r <- resolved(f)
-##   }, condition = function(w) w)
-##   stopifnot(inherits(res, "condition"))
+make_test(title = "Early signaling of warnings", args = list(lazy = c(FALSE, TRUE), earlySignal = c(FALSE, TRUE)), tags = c("earlySignal", "resolved", "resolve"), {
+  res <- tryCatch({
+    f <- future({ warning("watch out!") }, lazy = lazy, earlySignal = earlySignal)
+    stopifnot(inherits(f, "Future"))
+ 
+    r <- resolved(f)
+  
+    v <- tryCatch(value(f), warning = identity)
+    stopifnot(inherits(v, "warning"), conditionMessage(v) == "watch out!")
+
+    TRUE
+  }, error = identity)
+
+  stopifnot(inherits(res, "error") || isTRUE(res))
+})
+
+
+make_test(title = "Early signaling of messages", args = list(lazy = c(FALSE, TRUE), earlySignal = c(FALSE, TRUE)), tags = c("earlySignal", "resolved", "resolve"), {
+  res <- tryCatch({
+    f <- future({ message("hello world!") }, lazy = lazy, earlySignal = earlySignal)
+    stopifnot(inherits(f, "Future"))
+ 
+    r <- resolved(f)
+  
+    v <- tryCatch(value(f), message = identity)
+    stopifnot(inherits(v, "message"), conditionMessage(v) == "hello world!")
+
+    TRUE
+  }, error = identity)
+
+  stopifnot(inherits(res, "error") || isTRUE(res))
+})
+
+## ## Condition
+## f <- future({ signalCondition(simpleCondition("hmm")) }, lazy = TRUE)
+## res <- tryCatch({
+##   r <- resolved(f)
+## }, condition = function(w) w)
+## stopifnot(inherits(res, "condition"))
