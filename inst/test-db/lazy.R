dc9cff5f98a124437c116910e5a8fc8dec1ecddd
Henrik Bengtsson <hb@aroma-project.org>
2019-09-12 18:51:56 -0700

    TESTS: Add test for resolved() lauching lazy futures [#6]

---
+++ inst/test-db/lazy.R
@@ -0,0 +1,8 @@
+make_test(title = "resolved() on lazy futures", tags = c("resolved", "lazy"), {
+  f <- future(42, lazy = TRUE)
+  while (!resolved(f)) {
+    Sys.sleep(0.1)
+  }
+  v <- value(f)
+  stopifnot(identical(v, 42))
+})
