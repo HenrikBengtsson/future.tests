3c7100c8ec3f5024855bf99ebe4745e182192619
Henrik Bengtsson <hb@aroma-project.org>
2019-10-25 19:20:20 -0700

    TESTS: Test visibility of value()

---
+++ inst/test-db/visibility.R
@@ -0,0 +1,21 @@
+if (packageVersion("future") >= "1.14.0-9000") {
+
+make_test(title = "value() - visibility", tags = c("value", "visibility"), {
+  f <- future(42)
+  res <- withVisible({
+    value(f)
+  })
+  v <- res$value
+  stopifnot(identical(v, 42))
+  stopifnot(res$visible)
+
+  f <- future(invisible(42))
+  res <- withVisible({
+    value(f)
+  })
+  v <- res$value
+  stopifnot(identical(v, 42))
+  stopifnot(!res$visible)
+})
+
+}
